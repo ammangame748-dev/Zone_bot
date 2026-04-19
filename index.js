97841a2f7dfa1a56e73db56d02fe6196951c7607
ammangame748-dev <ammangame748@gmail.com>
2026-04-20 00:22:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1525,7 +1525,6 @@ if (command === 'ستريكي' || command === 'streak') {
                 }
 
                 // إرسال رسالة المباركة في الروم المحدد أو نفس الروم
-const targetCh = msg.guild.channels.cache.get(strkConf.streakChannel);
                 const streakEmbed = new EmbedBuilder()
                     .setDescription(`🔥 كفو **${msg.author.username}**! زاد الستريك تبعك وصار: \`${u.streakCount}\``)
                     .setColor('#ffbb00');
